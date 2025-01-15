89706e0048bdfa864b3a6e7b9c0d3d555b59bbf6
lifehardmod <kciimnida@gmail.com>
2025-01-15 11:26:38 +0900

    123

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,23 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
 
+function replaceEnvVariables() {
+  return {
+    name: "replace-env-variables",
+    transform(code, id) {
+      if (id.endsWith("firebase-messaging-sw.js")) {
+        return code
+          .replace("__VITE_FIREBASE_API_KEY__", JSON.stringify(process.env.VITE_FIREBASE_API_KEY))
+          .replace("__VITE_FIREBASE_AUTH_DOMAIN__", JSON.stringify(process.env.VITE_FIREBASE_AUTH_DOMAIN))
+          .replace("__VITE_FIREBASE_PROJECT_ID__", JSON.stringify(process.env.VITE_FIREBASE_PROJECT_ID))
+          .replace("__VITE_FIREBASE_STORAGE_BUCKET__", JSON.stringify(process.env.VITE_FIREBASE_STORAGE_BUCKET))
+          .replace("__VITE_FIREBASE_MESSAGING_SENDER_ID__", JSON.stringify(process.env.VITE_FIREBASE_MESSAGING_SENDER_ID))
+          .replace("__VITE_FIREBASE_APP_ID__", JSON.stringify(process.env.VITE_FIREBASE_APP_ID));
+      }
+      return code;
+    },
+  };
+}
 
 export default defineConfig({
   plugins: [
